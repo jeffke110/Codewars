29e8a76cfc7da3270900617641984dc7e9d29e0a
jeffke110 <jlkedda@gmail.com>
2024-12-24 10:08:44 -0500

    12/24/24

--- C/is-prime.c
+++ C/is-prime.c
@@ -4,13 +4,13 @@
 
 bool is_prime(int num)
 {
-    if(num < 1){ return false; }
-    for(int i = 2; i < sqrt(num); i++){
+    if(num <= 1){ return false; }
+    for(int i = 2; i <= sqrt(num); i++){
         if( num % i == 0){
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 int main(){
