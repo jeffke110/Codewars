8b0ceb85dabb5ed0d5ac6ac0bbd0709dfa12dbce
jeffke110 <jlkedda@gmail.com>
2024-12-24 09:38:37 -0500

    12/24/24

---
+++ C/find-maximum-and-minimum-values-of-a-list.c
@@ -0,0 +1,35 @@
+/*
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) 
+that receive a list of integers as input, and return the largest and lowest number in that list, respectively. 
+Each function returns one number.
+*/
+
+#include <stdio.h>
+
+int min(int* array, int arrayLength)
+{
+    int min = __INT_MAX__;
+    for(int i = 0; i < arrayLength; i++){
+        if(array[i] < min){
+            min = array[i];
+        }
+    }
+  return min;
+}
+
+int max(int* array, int arrayLength)
+{
+    int max = 0;
+    for(int i = 0; i < arrayLength; i++){
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+  return max;
+}
+
+int main(){
+    int array[] = {4,6,2,1,9,63,-134,566};
+    printf("%d", min(array, 8));
+}
+
